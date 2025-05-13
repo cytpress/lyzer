bb82b7222691bb96ecbe008ca543bd75496c0a16
cytpress <ganymede5035@gmail.com>
2025-05-13 20:20:09 +0800

    react router <Outlet> set

--- frontend/lygazsum/src/App.tsx
+++ frontend/lygazsum/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <div>
+      <header></header>
+      <main>
+        <Outlet />
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
--- frontend/lygazsum/src/routes.tsx
+++ frontend/lygazsum/src/routes.tsx
@@ -1,6 +1,16 @@
 import App from "./App";
 import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 export const routesConfig = createBrowserRouter([
-  { path: "/", element: <App />, children: [] },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
 ]);
