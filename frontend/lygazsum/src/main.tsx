bdf1ec34ceac54e7c4f6ce418edbd5e42c3abf0a
cytpress <ganymede5035@gmail.com>
2025-05-14 19:20:21 +0800

    create queryClient instance

--- frontend/lygazsum/src/main.tsx
+++ frontend/lygazsum/src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { routesConfig } from "./routes.tsx";
 
+const queryClient = new QueryClient();
 const router = routesConfig;
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
