35a97c618064ac0beddf50a040f8ae8c5fcec16d
cytpress <ganymede5035@gmail.com>
2025-05-21 08:45:46 +0800

    homepage committee tags added

--- frontend/lygazsum/src/components/GazetteListItem.tsx
+++ frontend/lygazsum/src/components/GazetteListItem.tsx
@@ -1,4 +1,5 @@
 import { HomePageGazetteItem } from "../types/models";
+import CommitteeTags from "./CommitteeTags";
 import { Link } from "react-router-dom";
 interface GazetteListItemProps {
   gazetteItem: HomePageGazetteItem;
@@ -6,15 +7,19 @@ interface GazetteListItemProps {
 export default function GazetteListItem({ gazetteItem }: GazetteListItemProps) {
   return (
     <li>
-      <div className="flex flex-row items-center">
-        <span className="flex-shrink-0">{gazetteItem.committee_names}</span>
-        <div className="flex flex-col items-start">
+      <div className="flex flex-row items-center justify-center">
+        <span className="flex-shrink-0">
+          <CommitteeTags committeeNames={gazetteItem.committee_names} />
+        </span>
+        <div className="flex flex-col items-start w-2/5 ">
           <Link to={`/detailedGazette/${gazetteItem.id}`}>
-            <p className="text-left">{gazetteItem.summary_title}</p>
+            <h3 className="font-medium text-left hover:text-blue-600">
+              {gazetteItem.summary_title}
+            </h3>
           </Link>
-          <p className="text-left">{gazetteItem.overall_summary_sentence}</p>
+          <p className="text-left line-clamp-2">{gazetteItem.overall_summary_sentence}</p>
         </div>
-        <span>{gazetteItem.meeting_date}</span>
+        <span className="flex-shrink-0">{gazetteItem.meeting_date}</span>
       </div>
     </li>
   );
